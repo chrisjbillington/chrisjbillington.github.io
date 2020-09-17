faf75e03c61f6800b2d9ff4d5d3324b3979f2fac
chrisjbillington <chrisjbillington@gmail.com>
2020-09-17 10:51:41 +1000

    Update DHHS 14d unknowns

--- victoria.py
+++ victoria.py
@@ -457,6 +457,7 @@ for j in range(LOOP_START, len(dates) + 1):
             ('2020-09-11', 94),
             ('2020-09-12', 83),
             ('2020-09-13', 82),
+            ('2020-09-14', 84),
         ]
     )
     unknowns_last_14d_dates = np.array(
