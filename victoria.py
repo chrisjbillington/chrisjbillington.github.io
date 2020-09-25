e72e1ceda178764ccebf1b371ec64a15e9ef3a62
chrisjbillington <chrisjbillington@gmail.com>
2020-09-26 09:29:40 +1000

    DHHS mysteries

--- victoria.py
+++ victoria.py
@@ -530,6 +530,7 @@ for j in range(LOOP_START, len(dates) + 1):
             ('2020-09-20', 41),
             ('2020-09-21', 37),
             ('2020-09-22', 34),
+            ('2020-09-23', 31),
         ]
     )
     unknowns_last_14d_dates = np.array(
